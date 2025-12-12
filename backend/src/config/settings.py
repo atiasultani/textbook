8cb587a88a6b038d56f03077feda37f8c4e97a72
atiasultani <160133182+atiasultani@users.noreply.github.com>
2025-12-12 22:21:12 +0500

    ok

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -1,7 +1,8 @@
 from pydantic_settings import BaseSettings
 from typing import Optional
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 
 class Settings(BaseSettings):
     # Database settings
